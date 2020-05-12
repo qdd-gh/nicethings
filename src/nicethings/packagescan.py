879203c797af3b7c1edb135a081d85fbf5d9e02c
qdd-gh <55011673+qdd-gh@users.noreply.github.com>
2020-05-11 22:24:40 -0400

    Add name, link, and version parsing

--- src/nicethings/packagescan.py
+++ src/nicethings/packagescan.py
@@ -1,16 +1,46 @@
+from distutils.version import LooseVersion
+from html.parser import HTMLParser
 from typing import List
 
 import attr
 
 
-@attr.s(auto_attribs=True, frozen=True)
+@attr.s(auto_attribs=True)
 class Distribution:
     name: str = ""
     link: str = ""
+    version: LooseVersion = None
+
+
+class PackageHtmlParser(HTMLParser):
+    def __init__(self, *, convert_charrefs=True):
+        super().__init__(convert_charrefs=convert_charrefs)
+        self.current_link = ""
+        self.distributions = []
+
+    def handle_starttag(self, tag, attrs):
+
+        if "a" != tag:
+            return
+
+        attrs_dict = dict(attrs)
+        self.current_link = attrs_dict["href"]
+
+    def handle_data(self, data):
+        if not self.current_link:
+            return
+
+        name = data.split("-")[0]
+        version = data.replace(name + "-", "")
+        version = LooseVersion(version)
+        dist = Distribution(name=data, link=self.current_link, version=version)
+        self.distributions.append(dist)
+        self.current_link = ""
 
 
 class PackageScan:
-
     @classmethod
     def scan(cls, html: str) -> List[Distribution]:
-        return [Distribution(name="cryptography", link="example.com")]
+        parser = PackageHtmlParser()
+        parser.feed(html)
+        return parser.distributions
--- test/test_packagescan.py
+++ test/test_packagescan.py
@@ -1,3 +1,5 @@
+from distutils.version import LooseVersion
+
 from nicethings.packagescan import PackageScan
 
 
@@ -8,3 +10,47 @@ def test_processes_html_for_list_of_distribution_names(shared_datadir):
 
     for distribution in distributions:
         assert "cryptography" in distribution.name.lower()
+
+
+def test_distribution_info_contains_version_info(shared_datadir):
+    html = (shared_datadir / "package.html").read_text()
+    distributions = PackageScan.scan(html)
+
+    for distribution in distributions:
+        version = distribution.version
+        assert version
+        assert version > LooseVersion("0.0.0")
+
+
+def test_all_distributions_are_parsed(shared_datadir):
+    html = (shared_datadir / "package.html").read_text()
+
+    from html.parser import HTMLParser
+
+    class HrefCounter(HTMLParser):
+        def __init__(self, *, convert_charrefs=True):
+            super().__init__(convert_charrefs=convert_charrefs)
+            self.hrefs = 0
+
+        def handle_starttag(self, tag, attrs):
+            if "a" != tag:
+                return
+
+            attrs_dict = dict(attrs)
+            if "href" in attrs_dict:
+                self.hrefs += 1
+
+    href_counter = HrefCounter()
+    href_counter.feed(html)
+
+    distributions = PackageScan.scan(html)
+    assert len(distributions) == href_counter.hrefs
+
+
+def test_distribution_information_includes_links(shared_datadir):
+    html = (shared_datadir / "package.html").read_text()
+    distributions = PackageScan.scan(html)
+    for distribution in distributions:
+        import urllib.parse
+
+        urllib.parse.urlparse(distribution.link)
